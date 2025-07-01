d3d680bc3b4d76b5567762941e8d68dafc485ac7
ARMANSYAH112 <ikanchana617@gmail.com>
2025-07-01 14:29:39 +0700

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,14 +1,22 @@
 return {
     {
-        name = "Grow a Garden GUI",
-        url = "https://raw.githubusercontent.com/Just3itx/Grow-A-Garden-API/main/GrowGUI.lua"
+        name = "XPERIA XAO | Curi Brainrot GUI",
+        url = "https://raw.githubusercontent.com/ARMANSYAH112/XperiaXao-Brainrot/main/loader.lua"
     },
     {
-        name = "Boxing Beta GUI",
+        name = "XPERIA XAO | Boxing Beta GUI",
         url = "https://raw.githubusercontent.com/ARMANSYAH112/XperiaXao-BoxingBeta/main/loader.lua"
     },
     {
-        name = "Knife Ability Test",
-        url = "https://pastebin.com/raw/ABCDEF12" -- ganti dengan yang asli kalau kamu punya
+        name = "XPERIA XAO | Grow A Garden Stock Bot",
+        url = "https://raw.githubusercontent.com/ARMANSYAH112/XperiaXao-GrowAGarden/main/stockbot.lua"
+    },
+    {
+        name = "XPERIA XAO | Tembus Tembok / Noclip",
+        url = "https://raw.githubusercontent.com/ARMANSYAH112/Tembus-tembok/main/xperia_xao.lua"
+    },
+    {
+        name = "XPERIA XAO | Auto Steal Brainrot (5 detik)",
+        url = "https://raw.githubusercontent.com/ARMANSYAH112/XperiaXao-Brainrot/main/autosteal.lua"
     }
 }
