e31d736a450d16eabb01d7857e1d8ae5d6b0bbb1
ARMANSYAH112 <ikanchana617@gmail.com>
2025-07-01 13:45:42 +0700

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        name = "Grow a Garden GUI",
+        url = "https://raw.githubusercontent.com/Just3itx/Grow-A-Garden-API/main/GrowGUI.lua"
+    },
+    {
+        name = "Boxing Beta GUI",
+        url = "https://raw.githubusercontent.com/ARMANSYAH112/XperiaXao-BoxingBeta/main/loader.lua"
+    },
+    {
+        name = "Knife Ability Test",
+        url = "https://pastebin.com/raw/ABCDEF12" -- ganti dengan yang asli kalau kamu punya
+    }
+}
